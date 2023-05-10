b057ad680af4a4202b8b14d99b287e00b67d6a36
Walter <walter.clark24@yahoo.com>
2023-05-10 10:13:13 -0400

    Secret Life of Objects

---
+++ SecretLifeOfObejcts.js
@@ -0,0 +1,8 @@
+// Methods are nothing more than properties that hold function values. This is a simple method:
+
+let rabbit = {}
+rabbit.speak = function(line){
+    console.log(`The rabbit says '${line}'`)
+}
+
+rabbit.speak("I'm alive.")
